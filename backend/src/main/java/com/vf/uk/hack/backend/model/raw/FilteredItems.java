df5cd87107b695a817f616105764d0842fa2b6d8
Adrian Jones <adrian.jones2@vodafone.com>
2022-11-22 14:15:22 +0000

    Update filter to sorted

--- backend/src/main/java/com/vf/uk/hack/backend/model/raw/FilteredItems.java
+++ backend/src/main/java/com/vf/uk/hack/backend/model/raw/FilteredItems.java
@@ -3,17 +3,44 @@ package com.vf.uk.hack.backend.model.raw;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
 
 @Data
 @NoArgsConstructor
 public class FilteredItems {
     private Set<String> model = new TreeSet<>();
     private Set<String> brand = new TreeSet<>();
-    private Set<String> screenSize = new TreeSet<>();
-    private Set<String> memoryInternal = new TreeSet<>();
+    private Set<String> screenSize = new TreeSet<>(new ScreenSizeComparator());
+    private Set<String> memoryInternal = new TreeSet<>(new MemoryComparator());
     private Set<String> colour = new TreeSet<>();
+
+
+  public class ScreenSizeComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+      double d1 = parseDouble(o1);
+      double d2 = parseDouble(o2);
+      return Double.compare(d1, d2);
+    }
+  }
+
+  public class MemoryComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+      if(o1.endsWith("GB")) {
+        o1 = o1.substring(0, o1.length()-2);
+      }
+      if(o2.endsWith("GB")) {
+        o2 = o2.substring(0, o2.length()-2);
+      }
+      return parseInt(o1) - parseInt(o2);
+    }
+  }
 
 
 /**
