2be0821f39ffd3bcf7d71798376332444cd5c310
Adrian Jones <adrian.jones2@vodafone.com>
2022-11-22 15:53:35 +0000

    files

--- frontend/src/components/HandsetCard/HandsetCard.tsx
+++ frontend/src/components/HandsetCard/HandsetCard.tsx
@@ -1,19 +1,32 @@
-import React, { useEffect, useState } from "react";
-import handSetType from "./HandsetCard.type";
+import "./HandsetsCard.css";
 
 const HandsetsCard = ({ handsets, loading, hasError }: any) => {
   console.log(handsets);
 
   return (
     <>
-      {/* <h1>{handsets}</h1> */}
       {hasError && <h1>Error</h1>}
       {loading ? (
         <h1>Loading</h1>
       ) : (
         handsets.map((handset: any) => (
-          // console.log('>', handset)
-          <div key={handset.id}>{handset.id}</div>
+          <>
+            <div className="card">
+              <div className="card-content">
+                <img
+                  className="imageCard"
+                  src={handset.imageURL}
+                  alt={handsets.displayName}
+                />
+              </div>
+              <div className="card-content">
+                <p className="secondaryText">{handset.brand}</p>
+                <h4 className="titleText">{handset.displayName}</h4>
+                <h4 className="titleText">{handset.memoryInternal}</h4>
+                <p className="secondaryText">{handset.displayDescription}</p>
+              </div>
+            </div>
+          </>
         ))
       )}
     </>
